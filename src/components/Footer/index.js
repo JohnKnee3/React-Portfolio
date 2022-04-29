ea1a8a74f9fafac2221d5e94a45f1d4d601bc361
john.a.clark3 <john.a.clark3@gmail.com>
2022-04-29 08:54:51 -0600

    copy pasted in from class
    time to convert and get running starting with about

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,5 +1,11 @@
 import React from "react";
 
-function Footer() {}
+function Footer() {
+  return (
+    <div>
+      <p>contact me</p>
+    </div>
+  );
+}
 
 export default Footer;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import Nav from "../Nav";
 
-function Header() {}
+function Header() {
+  const [currentPage, setCurrentPage] = useState("Home");
+
+  // TODO: Add a comment describing the functionality of this method
+  // This is checking the name of current page is and matches it to watch you clicked
+  const renderPage = () => {
+    if (currentPage === "Home") {
+      return <Home />;
+    }
+    if (currentPage === "About") {
+      return <About />;
+    }
+    if (currentPage === "Blog") {
+      return <Blog />;
+    }
+    return <Contact />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      <h1>John Clark</h1>
+      <Nav currentPage={currentPage} handlePageChange={handlePageChange} />
+      {renderPage()}
+    </div>
+  );
+}
 
 export default Header;
