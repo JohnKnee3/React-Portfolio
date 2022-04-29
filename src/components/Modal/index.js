acb575fa186fa33c6e02de3d1b9c512fbeea0b91
john.a.clark3 <john.a.clark3@gmail.com>
2022-04-29 12:05:16 -0600

    testing to see if I can get a modal

---
+++ src/components/Modal/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Modal = ({ onClose, currentPhoto }) => {
+  const { name, description, index } = currentPhoto;
+
+  return (
+    <div className="modalBackdrop">
+      <div className="modalContainer">
+        <h3 className="modalTitle">{name}</h3>
+        <img
+          src={require(`../../images/commercial/${index}.jpg`).default}
+          alt="current display"
+        />
+        <p>{description}</p>
+        <button onClick={onClose} type="button">
+          Close this modal
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
+import Modal from "../Modal";
 
 function Project() {
-  const projects = [
+  const [currentPhoto, setCurrentPhoto] = useState();
+  const [projects] = useState([
     {
-      name: "Grocery aisle",
+      name: "Copy Cat Games",
 
       description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie",
@@ -32,20 +34,40 @@ function Project() {
       description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie",
     },
-  ];
+  ]);
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const toggleModal = (image, i) => {
+    console.log("I am image", image);
+
+    // current photo
+    setCurrentPhoto({ ...image, index: i });
+
+    setIsModalOpen(!isModalOpen);
+  };
 
   return (
-    <div className="flex-row">
-      <h1>Projects</h1>
-      <ul>
-        {projects.map((work, index) => (
-          <li key={index}>
-            <img src={require(`../../images/commercial/${index}.jpg`)} />
-            Name: {work.name}
-            <p>description: {work.description}</p>{" "}
-          </li>
-        ))}
-      </ul>
+    <div>
+      {isModalOpen && (
+        <Modal currentPhoto={currentPhoto} onClose={toggleModal} />
+      )}
+      <div className="flex-row">
+        <h1>Projects</h1>
+        <ul>
+          {projects.map((project, index) => (
+            <div key={index}>
+              <img
+                src={require(`../../images/commercial/${index}.jpg`)}
+                alt={project.name}
+                onClick={() => toggleModal(project, index)}
+                key={project.name}
+              />
+              <p>{project.name}</p>
+            </div>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
