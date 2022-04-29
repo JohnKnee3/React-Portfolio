4d453350f75d347eb1540ff5a5533d955a0f4416
john.a.clark3 <john.a.clark3@gmail.com>
2022-04-29 09:49:43 -0600

    the about bones are set up
    found a way to bring in an image

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,7 +1,28 @@
 import React from "react";
+import turkey from "../../images/turkey.jpg";
 
 function About() {
-  return <div>Hey I am Johnny</div>;
+  return (
+    <div>
+      <section id="about-me">
+        <div className="about-me">
+          <h2>About Me</h2>
+          <img
+            src={turkey}
+            alt="image of a turkey"
+            className="img-thumbnail mx-1"
+            key={"turkey.jpg"}
+          />
+          <p>
+            I am a Turkey, well not exactly. But I once saw a Turkey all fluffed
+            out chasing a bus and I was like "Yeah....I get you. I think I
+            matter sometimes too." It was a beautiful moment reminding me of my
+            importance.
+          </p>
+        </div>
+      </section>
+    </div>
+  );
 }
 
 export default About;
