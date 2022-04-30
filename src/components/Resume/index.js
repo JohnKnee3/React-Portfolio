1dd44fe5f38adcaf6e0d91082ce1844a6d7869fc
john.a.clark3 <john.a.clark3@gmail.com>
2022-04-30 12:06:38 -0600

    tweaked resume page

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -2,8 +2,14 @@ import React from "react";
 import MyResume from "../../assets/resumes/John-Clark.pdf";
 function Resume() {
   return (
-    <div class="resume-container">
-      <h2 class="resume">font-end technologies</h2>
+    <div className="resume-container">
+      <div>
+        <h2>download resume </h2>
+        <a href={MyResume} download>
+          <img src="https://img.icons8.com/cute-clipart/64/000000/pdf.png" />
+        </a>
+      </div>
+      <h2 className="resume">font-end technologies</h2>
       <ul>
         <li>HTML</li>
         <li>CSS</li>
@@ -14,7 +20,7 @@ function Resume() {
         <li>Responsive Web Design</li>
         <li>Templating language (Handlebars)</li>
       </ul>
-      <h2 class="resume">back-end technologies</h2>
+      <h2 className="resume">back-end technologies</h2>
       <ul>
         <li>Node.js</li>
         <li>Express.js</li>
@@ -22,10 +28,6 @@ function Resume() {
         <li>NoSQL (MongoDB, Mongoose)</li>
         <li>API's (web, third-party, RESTful, server-side)</li>
       </ul>
-      <a href={MyResume} download>
-        <img src="https://img.icons8.com/cute-clipart/64/000000/pdf.png" />
-      </a>
-      <p>Resume</p>
     </div>
   );
 }
