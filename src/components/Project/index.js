3828e12311e0d30bb714418273265793b991060b
john.a.clark3 <john.a.clark3@gmail.com>
2022-04-30 13:11:10 -0600

    color change
    showed it to the GF and she had some notes :)

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -9,12 +9,16 @@ function Project() {
       linkPage: "https://morning-spire-87893.herokuapp.com/",
       linkGit: "https://github.com/JohnKnee3/Copy-Cat-Games",
       description: "A silly little site for a fictional board game company.",
+      tech: "Bootstrap/JavaScript/Node.js/",
+      tech2: "Express.js/MySQL/Handlebars",
     },
     {
       name: "Crypto Checker",
       linkPage: "https://bullmoosedev.github.io/Crypto-Checker/",
       linkGit: "https://github.com/BullMooseDev/Crypto-Checker",
       description: "A site that helps people track the top trending Cryptos",
+      tech: "HTML/CSS/Materialize/",
+      tech2: "JavaScript/API",
     },
     {
       name: "Track My Budget",
@@ -22,6 +26,8 @@ function Project() {
       linkGit: "https://github.com/JohnKnee3/Track-My-Budget",
       description:
         "Track your budget while in on or offline.  Can convert to a PWA as well.",
+      tech: "IndexDB/Service Workers/",
+      tech2: "Web Manifest",
     },
     {
       name: "Yet Another Tech Blog",
@@ -29,18 +35,24 @@ function Project() {
       linkGit: "https://github.com/JohnKnee3/Yet-Another-Tech-Blog",
       description:
         "A site where tech lovers can chat about all the love they love.",
+      tech: "HTML/CSS/Express/MySQL/",
+      tech2: "JavaScript",
     },
     {
       name: "Weather Dashboard",
       linkPage: "https://johnknee3.github.io/Weather-Dashboard/",
       linkGit: "https://github.com/JohnKnee3/Weather-Dashboard",
       description: "A site where you can track the weather by city name.",
+      tech: "HTML/CSS/JavaScript/",
+      tech2: "API",
     },
     {
       name: "Note Taker",
       linkPage: "https://arcane-beyond-71149.herokuapp.com/",
       linkGit: "https://github.com/JohnKnee3/Note-Taker",
       description: "A handy-dandy little note taking site.",
+      tech: "Node/Express/AJAX/",
+      tech2: "jQuery",
     },
   ]);
 
@@ -62,20 +74,30 @@ function Project() {
       )}
       <div>
         <h1 className="my-name">Projects</h1>
-        <ul>
-          {projects.map((project, index) => (
-            <div className="project-container" key={index}>
-              {/* <p>{project.name}</p> */}
-              <img
-                src={require(`../../assets/images/websites/tiny/${index}.jpg`)}
-                className="img-thumbnail-project mx-1"
-                alt={project.name}
-                onClick={() => toggleModal(project, index)}
-                key={project.name}
-              />
+
+        {projects.map((project, index) => (
+          <div className="project-container" key={index}>
+            {/* <p>{project.name}</p> */}
+            <img
+              src={require(`../../assets/images/websites/tiny/${index}.jpg`)}
+              className="img-thumbnail-project mx-1"
+              alt={project.name}
+              onClick={() => toggleModal(project, index)}
+              key={project.name}
+            />
+            <div
+              className="centered "
+              onClick={() => toggleModal(project, index)}
+            >
+              <h2 className="project-title">{project.name}</h2>
+              <p>
+                {project.tech}
+                <br />
+                {project.tech2}{" "}
+              </p>
             </div>
-          ))}
-        </ul>
+          </div>
+        ))}
       </div>
     </div>
   );
