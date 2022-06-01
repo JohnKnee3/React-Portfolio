b22e286756b70996ba5d1b8c08b9a2c554229649
john.a.clark3 <john.a.clark3@gmail.com>
2022-06-01 14:45:11 -0600

    got the about page to diplay

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import Nav from "../Nav";
 import About from "../About";
 import Project from "../Project";
@@ -8,19 +9,19 @@ import Resume from "../Resume";
 function Header() {
   const [currentPage, setCurrentPage] = useState("About");
 
-  const renderPage = () => {
-    if (currentPage === "About") {
-      return <About />;
-    }
-    if (currentPage === "Project") {
-      return <Project />;
-    }
+  // const renderPage = () => {
+  //   if (currentPage === "About") {
+  //     return <About />;
+  //   }
+  //   if (currentPage === "Project") {
+  //     return <Project />;
+  //   }
 
-    if (currentPage === "Contact") {
-      return <Contact />;
-    }
-    return <Resume />;
-  };
+  //   if (currentPage === "Contact") {
+  //     return <Contact />;
+  //   }
+  //   return <Resume />;
+  // };
 
   const handlePageChange = (page) => setCurrentPage(page);
 
@@ -28,9 +29,58 @@ function Header() {
     <div>
       <div className="header-container">
         <h1 className="my-name my-real-name">John Clark</h1>
-        <Nav currentPage={currentPage} handlePageChange={handlePageChange} />
+        {/* <Nav currentPage={currentPage} handlePageChange={handlePageChange} /> */}
+        <ul className="nav nav-tabs">
+          <li className="nav-item">
+            <Link
+              to="/"
+              OnClick={() => handlePageChange("About")}
+              className={
+                currentPage === "About" ? "nav-link active" : "nav-link"
+              }
+            >
+              About
+            </Link>
+          </li>
+          <li className="nav-item">
+            <a
+              href="#project"
+              onClick={() => handlePageChange("Project")}
+              //*  TODO: BONUS: Add a comment explaining what kind of operator this is and what it is checking for
+
+              className={
+                currentPage === "Project" ? "nav-link active" : "nav-link"
+              }
+            >
+              Projects
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              href="#contact"
+              onClick={() => handlePageChange("Contact")}
+              className={
+                currentPage === "Contact" ? "nav-link active" : "nav-link"
+              }
+            >
+              Contact
+            </a>
+          </li>
+
+          <li className="nav-item">
+            <a
+              href="#resume"
+              onClick={() => handlePageChange("Resume")}
+              className={
+                currentPage === "Resume" ? "nav-link active" : "nav-link"
+              }
+            >
+              Resume
+            </a>
+          </li>
+        </ul>
       </div>
-      <div>{renderPage()}</div>
+      {/* <div>{renderPage()}</div> */}
     </div>
   );
 }
