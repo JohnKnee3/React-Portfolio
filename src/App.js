9ed0aa770e95e8b0ac4d4d3d4ec73c1ca317bc0b
john.a.clark3 <john.a.clark3@gmail.com>
2022-06-01 14:36:21 -0600

    got the app.js for the router in

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import About from "./components/About";
+import Project from "./components/Project";
+import Contact from "./components/Contact";
+import Resume from "./components/Resume";
 
 function App() {
   return (
-    <div className="content">
-      <Header></Header>
+    <Router>
+      <div className="content">
+        <Header />
+        <Routes>
+          {/* <Route path="/" element={<Home />} /> */}
+          <Route path="/" element={<About />} />
+          <Route path="/Project" element={<Project />} />
+          <Route path="/Contact" element={<Contact />} />
+          <Route path="Resume" element={<Resume />} />
+        </Routes>
 
-      <Footer></Footer>
-    </div>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
